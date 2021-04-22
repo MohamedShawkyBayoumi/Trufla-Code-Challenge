ff10dc0aa20fecb99ee111d84d94f11e242fb263
Mohamed Shawky <mohamedshawkybayoumi@gmail.com>
2021-04-22 13:01:43 +0200

    feat: mock json API

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Home from './pages/Home';
 
 function App() {
+
+  const getData = async () => {
+    try {
+      let response = await fetch("http://localhost:8000/data");
+      let result = await response.json();
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="app">
       <Home />

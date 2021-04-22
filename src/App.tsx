995ab276213b00db891ba761bf42bb3e869b1aab
Mohamed Shawky <mohamedshawkybayoumi@gmail.com>
2021-04-22 15:00:00 +0200

    feat: handle collapse logic and filter interests

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Home from './pages/Home';
 
+type Users = {
+  id: number;
+  name: string;
+  following: number[];
+  interests?: number[];
+}
+
+type Interests = {
+  id: number;
+  name: string;
+}
+
+export interface DataTypes {
+  users: Array<Users>;
+  interests: Array<Interests>;
+}
+
 function App() {
+
+  const [users, setUsers] = useState<Users[]>([]);
+  const [interests, setInterests] = useState<Interests[]>([]);
 
   const getData = async () => {
     try {
       let response = await fetch("http://localhost:8000/data");
-      let result = await response.json();
-      console.log(result);
+      let result = await response.json() as DataTypes;
+      setUsers(result?.users);
+      setInterests(result?.interests);
     } catch (e) {
       console.log(e);
     }
@@ -20,7 +41,7 @@ function App() {
 
   return (
     <div className="app">
-      <Home />
+      <Home users={users} interests={interests} />
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { AppBar, Collapse, List, ListItem, ListItemIcon, ListItemText, ListSubheader, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Box, Collapse, List, ListItem, ListItemIcon, ListItemText, ListSubheader, makeStyles, Toolbar, Typography } from '@material-ui/core';
 import { AccountBox, ExpandLess, ExpandMore, Stars } from '@material-ui/icons';
+import { DataTypes } from '../App';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -13,13 +14,33 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-function Home() {
+type linkedData = {
+    [key: string]: DataTypes['interests'];
+
+}
+
+function Home({ users = [], interests = [] }: DataTypes) {
     const classes = useStyles();
     const [open, setOpen] = useState(true);
+    const [activeID, setActiveID] = useState<null | number>(null);
+    const [linkedData, setLinkedData] = useState<linkedData>({});
 
-    const handleClick = () => {
-        setOpen(!open);
+    const handleClick = (id: number, userIndex: number) => {
+        const usersInterests = users[userIndex].interests;
+        const filteredInterests = interests.filter((interest) => usersInterests?.find(item => item == interest.id));
+        console.log(filteredInterests);
+
+        if(linkedData[`${id}_${userIndex}`]){
+            const filteredLinkedData = Object.assign({}, linkedData);
+            delete filteredLinkedData[`${id}_${userIndex}`];
+            setLinkedData(filteredLinkedData);
+        } else {
+            setLinkedData({ ...linkedData, [`${id}_${userIndex}`]: filteredInterests });
+        }
+        setActiveID(id);
     };
+
+    console.log('linkedData linkedData linkedData', linkedData);
     return (
         <div>
             <AppBar position="static">
@@ -34,40 +55,40 @@ function Home() {
                 aria-labelledby="nested-list-subheader"
                 subheader={
                     <ListSubheader component="div" id="nested-list-subheader">
-                        Users
+                        Users with their interests
                     </ListSubheader>
                 }
                 className={classes.root}
                 >
-                <ListItem button>
-                    <ListItemIcon>
-                        <AccountBox />
-                    </ListItemIcon>
-                    <ListItemText primary="Sent mail" />
-                </ListItem>
-                <ListItem button>
-                    <ListItemIcon>
-                        <AccountBox />
-                    </ListItemIcon>
-                    <ListItemText primary="Drafts" />
-                </ListItem>
-                <ListItem button onClick={handleClick}>
-                    <ListItemIcon>
-                        <AccountBox />
-                    </ListItemIcon>
-                    <ListItemText primary="Inbox" />
-                    {open ? <ExpandLess /> : <ExpandMore />}
-                </ListItem>
-                <Collapse in={open} timeout="auto" unmountOnExit>
-                    <List component="div" disablePadding>
-                        <ListItem button className={classes.nested}>
-                            <ListItemIcon>
-                                <Stars />
-                            </ListItemIcon>
-                            <ListItemText primary="Starred" />
-                        </ListItem>
-                    </List>
-                </Collapse>
+                    {users.map(({ id, name, following, interests = [] }, userIndex) => {
+                        return(
+                            <Box key={id}>
+                                <ListItem button={interests.length > 0 ? true : true} onClick={() => interests.length > 0 ? handleClick(id, userIndex) : {}}>
+                                    <ListItemIcon>
+                                        <AccountBox />
+                                    </ListItemIcon>
+                                    <ListItemText primary={name} />
+                                    {interests.length ? (
+                                        <>
+                                            {linkedData[`${id}_${userIndex}`] ? <ExpandLess /> : <ExpandMore />}
+                                        </>
+                                    ) : null}
+                                </ListItem>
+                                {Object.keys(linkedData).length && linkedData[`${id}_${userIndex}`] ? linkedData[`${id}_${userIndex}`].map((interest, index) => (
+                                    <Collapse in={open} timeout="auto" unmountOnExit key={interest.id}>
+                                        <List component="div" disablePadding>
+                                            <ListItem button className={classes.nested}>
+                                                <ListItemIcon>
+                                                    <Stars />
+                                                </ListItemIcon>
+                                                <ListItemText primary={interest.name} />
+                                            </ListItem>
+                                        </List>
+                                    </Collapse>
+                                )): null}
+                            </Box>
+                        )
+                    })}
             </List>
         </div>
     )
